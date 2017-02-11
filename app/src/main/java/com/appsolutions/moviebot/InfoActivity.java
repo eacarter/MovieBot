2bb99ffcf26d442ef725cd98ad4906f5cdf14332
Eric Carter <eacarter@mtu.edu>
2017-02-11 14:34:23 -0500

    InfoActivity partially integrated need to finish: * XML formating * Image processing from json url * Sliding menu formatting * Menu options and integration of other api's

--- app/src/main/java/com/appsolutions/moviebot/InfoActivity.java
+++ app/src/main/java/com/appsolutions/moviebot/InfoActivity.java
@@ -1,10 +1,14 @@
 package com.appsolutions.moviebot;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,22 +18,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class InfoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     TextView title;
-    Image poster;
+    ImageView poster;
     TextView year;
     TextView rated;
     TextView released;
     TextView runtime;
     TextView genre;
+    TextView director;
+    TextView actor;
+    TextView plot;
 
 
     @Override
@@ -39,12 +53,39 @@ public class InfoActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        title = (TextView)findViewById(R.id.title);
+        poster = (ImageView)findViewById(R.id.poster);
+        year = (TextView)findViewById(R.id.year);
+        rated = (TextView)findViewById(R.id.rated);
+        released = (TextView)findViewById(R.id.released);
+        runtime = (TextView)findViewById(R.id.runtime);
+        genre = (TextView)findViewById(R.id.genre);
+        director = (TextView)findViewById(R.id.director);
+        actor = (TextView)findViewById(R.id.actors);
+        plot = (TextView)findViewById(R.id.plot);
+
+
         Intent intent = getIntent();
         String jString = intent.getStringExtra("jsonObject");
 
         JSONObject jOb = null;
         try {
             jOb = new JSONObject(jString);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            title.setText(jOb.getString("Title"));
+            //poster.setImageBitmap(getBitmapFromUrl(jOb.getString("Poster")));
+            year.setText(jOb.getString("Year"));
+            rated.setText(jOb.getString("Rated"));
+            released.setText(jOb.getString("Released"));
+            runtime.setText(jOb.getString("Runtime"));
+            genre.setText(jOb.getString("Genre"));
+            director.setText(jOb.getString("Director"));
+            actor.setText(jOb.getString("Actors"));
+            plot.setText(jOb.getString("Plot"));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -129,5 +170,23 @@ public class InfoActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private static Bitmap getBitmapFromUrl(String src){
+        try {
+            Log.e("src",src);
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap","returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
     }
 }
